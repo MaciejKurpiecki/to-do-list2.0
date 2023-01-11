d6db57f6d6db9d50ded548447e4167cacc606f36
Maciej Kurpiecki <recon4173@wp.pl>
2023-01-11 17:10:09 +0100

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,72 @@
+{
+    const tasks = [];
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+    const toggleStatus = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+    const resetInputField = () => {
+        const inputField = document.querySelector(".js-newTask");
+
+        inputField.value = '';
+        inputField.focus();
+    };
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => removeTask(index))
+        });
+
+        const toggleStatusButtons = document.querySelectorAll(".js-done");
+
+        toggleStatusButtons.forEach((toggleStatusButton, index) => {
+            toggleStatusButton.addEventListener("click", () => toggleStatus(index))
+        });
+    };
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li class="list__item${task.done ? " list__item--done" : ""}">
+            <button class="js-done list__doneButton${task.done ? " list__doneButton--done" : ""}"></button> 
+            <button class="js-remove list__removeButton"></button>          
+            <span class="list__task">${task.content}</span>
+                        </li>`;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
+    };
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return resetInputField();
+        }
+        addNewTask(newTaskContent);
+        resetInputField();
+    };
+    const init = () => {
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+
+        render();
+    };
+    init();
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# To-Do List
+
+**To-Do List** is a website to create your own to-do list. Website allows to add new tasks, mark tasks as completed/not completed and remove tasks form the list.
+
+https://maciejkurpiecki.github.io/To-Do-List/
